2861e8e9ece5f257f64b60ead8253668f9757a2d
jugalkariya94 <jugalkariya@gmail.com>
2024-06-29 16:13:19 +0530

    Update user factory and add user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Ramsey\Uuid\Uuid;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -24,11 +25,18 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'uuid' => Uuid::uuid4()->toString(),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'is_admin' => false,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => $this->faker->dateTime(),
+            'password' => Hash::make('password'), // password
+            'avatar' => null,
+            'address' => $this->faker->address,
+            'phone_number' => $this->faker->phoneNumber,
+            'is_marketing' => $this->faker->boolean,
+            'last_login_at' => $this->faker->dateTime(),
         ];
     }
 
@@ -41,4 +49,17 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    /**
+     * Indicate that the model is an admin.
+     */
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_admin' => true,
+            'email' => 'admin@buckhill.co.uk',
+            'password' => Hash::make('password'), // password
+        ]);
+    }
+
 }
