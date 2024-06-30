3f7843df78b0a3b3165f74f41fcd88312d7686d2
Jugal Kariya <jugalkariya@gmail.com>
2024-06-30 23:14:31 +0530

    [Frontend] Made basic navbar design for the frontend, need to separate navbar into different component

--- resources/js/layout/App.vue
+++ resources/js/layout/App.vue
@@ -1,13 +1,50 @@
 <template>
-    Hello World
+    <v-app>
+        <v-app-bar flat color="primary" class="d-flex align-center justify-center">
+            <v-container class="mx-auto py-0 fill-height d-flex justify-space-between text-white">
+                <v-avatar
+                    class="d-flex"
+                    size="32"
+                >
+                </v-avatar>
+
+                <v-row justify="center" no-gutters>
+                    <v-btn
+                        v-for="link in links"
+                        :key="link"
+                        class="mx-2"
+                        color="white"
+                        rounded="xl"
+                        variant="text"
+                    >
+                        {{ link }}
+                    </v-btn>
+                </v-row>
+                <v-spacer></v-spacer>
+            </v-container>
+        </v-app-bar>
+
+    </v-app>
 </template>
+
+<script setup>
+const links = [
+    'Products',
+    'Promotions',
+    'Blog',
+]
+</script>
 
 <script>
 export default {
-    name: "App"
+    data: () => ({
+        links: [
+            'Products',
+            'Promotions',
+            'Blog',
+        ],
+
+    }),
+
 }
 </script>
-
-<style scoped>
-
-</style>
--- resources/js/vuetify.ts
+++ resources/js/vuetify.ts
@@ -2,9 +2,60 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+const BuckhillGreen = {
+    dark: false,
+    colors: {
+        background: '#FFFFFF',
+        surface: '#FFFFFF',
+        'surface-variant': '#424242',
+        'on-surface-variant': '#EEEEEE',
+        primary: '#4EC690',
+        'primary-darken-1': '#3700B3',
+        secondary: '#03DAC6',
+        'secondary-darken-1': '#018786',
+        error: '#B00020',
+        info: '#1976D2',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+        light: '#EDF5F1',
+        dark: '#2DB479',
+        mainInfo: '#2196F3',
+        grey: '#B6C4C1',
+        'grey-200': '#ECF0EF',
+        'grey-300': '#DDE3E2',
+        'grey-500': '#95A7A3',
+        'grey-600': '#69817B',
+    },
+    variables: {
+        'border-color': '#000000',
+        'border-opacity': 0.12,
+        'high-emphasis-opacity': 0.87,
+        'medium-emphasis-opacity': 0.60,
+        'disabled-opacity': 0.38,
+        'idle-opacity': 0.04,
+        'hover-opacity': 0.04,
+        'focus-opacity': 0.12,
+        'selected-opacity': 0.08,
+        'activated-opacity': 0.12,
+        'pressed-opacity': 0.12,
+        'dragged-opacity': 0.08,
+        'theme-kbd': '#212529',
+        'theme-on-kbd': '#FFFFFF',
+        'theme-code': '#F5F5F5',
+        'theme-on-code': '#000000',
+    }
+}
+
+
 const vuetify = createVuetify({
     components,
-    directives
+    directives,
+    theme: {
+        defaultTheme: 'BuckhillGreen',
+            themes: {
+            BuckhillGreen,
+        },
+    },
 });
 
 export default vuetify;
