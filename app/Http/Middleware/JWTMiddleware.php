59385e8f7a1d36191e69549f5271c61901272b64
jugalkariya94 <jugalkariya@gmail.com>
2024-07-03 12:37:12 +0530

    [JWT Middleware] - Changed the error key - Added AUTH_GUARD key to .env.example to make it default auth guard

--- app/Http/Middleware/JWTMiddleware.php
+++ app/Http/Middleware/JWTMiddleware.php
@@ -17,7 +17,7 @@ class JWTMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if (Auth::guard('api')->guest()) {
-            return response()->json(['success' => false, 'message' => 'Unauthorized'], 401);
+            return response()->json(['success' => false, 'error' => 'Unauthorized'], 401);
         }
         return $next($request);
     }
