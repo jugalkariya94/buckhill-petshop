8e6f2e421392fcda9b12ed580d3b7a7c705b8d5f
jugalkariya94 <jugalkariya@gmail.com>
2024-07-03 14:51:37 +0530

    [Frontend - Setup] - Updated settings and installed supporting packages for Vue and Vuetify - Made small adjustments regarding typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,40 @@
-import pluginVue from 'eslint-plugin-vue'
-export default [
-    // add more generic rulesets here, such as:
-    // js.configs.recommended,
-    ...pluginVue.configs['flat/recommended'],
+import js from '@eslint/js'
+import eslintPluginVue from 'eslint-plugin-vue'
+import ts from 'typescript-eslint'
+
+export default ts.config(
+    js.configs.recommended,
+    ...ts.configs.recommended,
+    ...ts.configs.stylistic,
+    ...eslintPluginVue.configs['flat/recommended'],
     {
-        rules: {
-            // override/add rules settings here, such as:
-            // 'vue/no-unused-vars': 'error'
-        }
-    }
-]
+        languageOptions: {
+            parserOptions: {
+                parser: ts.parser
+            }
+        },
+        settings: {
+            // This will do the trick
+            'import/parsers': {
+                espree: ['.js', '.cjs', '.mjs', '.jsx'],
+                '@typescript-eslint/parser': ['.ts', '.tsx'],
+                'vue-eslint-parser': ['.vue'],
+            },
+            'import/resolver': {
+                typescript: true,
+                node: true,
+                alias: {
+                    map: [
+                        ['@', './resources/js'],
+                        ['~', './node_modules'],
+                    ],
+                    extensions: ['.js', '.ts', '.jsx', '.tsx', '.vue'],
+                },
+            },
+            vite: {
+                configPath: './vite.config.ts',
+            },
+        },
+    },
+
+)
