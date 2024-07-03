e4dd2650e16387ca138778023fda064fe55eecdd
Jugal Kariya <jugalkariya@gmail.com>
2024-07-03 21:17:26 +0530

    [Frontend Homepage] - Changes for logo and menu

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,55 +1,91 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
 import logo from '@/assets/images/Vector.svg';
+import brandName from '@/assets/images/petson..svg';
 
 export default defineComponent({
-  data() {
-    return {
-      logo,
-      links: [
-        'Products',
-        'Promotions',
-        'Blog',
-      ]
+    data() {
+        return {
+            logo,
+            brandName,
+            links: [
+                {
+                    name: 'Products',
+                    route: '/products',
+                    icon: 'mdi-chevron-down'
+                },
+                {
+                    name: 'Promotions',
+                    route: '/promotions'
+                },
+                {
+                    name: 'Blog',
+                    route: '/blog'
+                }
+            ]
+        }
     }
-  }
 
 })
 </script>
 
 
 <template>
-  <v-app-bar flat color="primary" class="d-flex align-center justify-center">
-    <v-container class="mx-auto py-0 fill-height d-flex justify-space-between text-white">
-      <v-avatar
-          class="d-flex"
-          size="32"
-      >
-        <v-img
-            alt="Logo"
-            class="shrink mr-2"
-            contain
-            :src="logo"
-            transition="scale-transition"
-            width="30"
-        />
-      </v-avatar>
+    <v-app-bar flat color="primary" class="d-flex align-center justify-center">
+        <v-container class="mx-auto py-0 fill-height d-flex justify-space-between text-white">
+            <v-row justify="center" no-gutters>
+                <v-app-bar-nav-icon class="mx-20">
+                    <v-img
+                        alt="Logo"
+                        class="d-flex shrink mr-2 fill-height"
+                        contain
+                        :src="logo"
+                        transition="scale-transition"
+                        width="25"
+                    />
+                    <span class="text-lowercase">petson.</span>
+                </v-app-bar-nav-icon>
+            </v-row>
+            <v-spacer></v-spacer>
+            <v-row justify="center" no-gutters>
+                <v-btn
+                    v-for="link in links"
+                    :key="link.name"
+                    class="mx-2"
+                    color="white"
+                    variant="text"
+                >
+                    {{ link.name }} <v-icon :icon="link.icon" v-if="link.icon"></v-icon>
+                </v-btn>
+            </v-row>
+            <v-spacer></v-spacer>
+            <v-row justify="end" no-gutters>
+                <v-btn
+                    class="mx-2"
+                    color="white"
+                    variant="outlined"
+                >
+                    <v-icon icon="mdi-cart"></v-icon>
+                    Cart (0)
+                </v-btn>
+                <v-btn
+                    class="mx-2"
+                    color="white"
+                    variant="text"
+                    large
+                >
+                    Login
+                </v-btn>
 
-      <v-row justify="center" no-gutters>
-        <v-btn
-            v-for="link in links"
-            :key="link"
-            class="mx-2"
-            color="white"
-            rounded="xl"
-            variant="text"
-        >
-          {{ link }}
-        </v-btn>
-      </v-row>
-      <v-spacer></v-spacer>
-    </v-container>
-  </v-app-bar>
+
+                <v-avatar
+                    class="d-flex"
+                    size="32"
+                >
+                </v-avatar>
+            </v-row>
+        </v-container>
+    </v-app-bar>
 </template>
 
 
--- resources/js/vuetify.ts
+++ resources/js/vuetify.ts
@@ -2,6 +2,11 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+
+
+
 const BuckhillGreen = {
     dark: false,
     colors: {
